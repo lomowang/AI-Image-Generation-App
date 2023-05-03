7c11418c26cb0f7b0f0dc4c8f93ea7b290305500
lomowang <frees1102@gmail.com>
2023-05-03 23:05:58 +0800

    20230503

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,3 +1,5 @@
+
+import FileSaver from 'file-saver'
 import { surpriseMePrompts } from '../constants'
 
 
@@ -5,7 +7,12 @@ export function getRandomPrompt(prompt){
     const randomIndex = Math.floor(Math.random() *surpriseMePrompts.length);
     const randomPrompt = surpriseMePrompts[randomIndex];
 
-    if(randomPrompt === prompt) return getRandomPrompt(prompt)
+    if(randomPrompt === prompt) return getRandomPrompt(prompt);
 
-    return randomPrompt
+    return randomPrompt;
+}
+
+
+export async function downloadImage (_id, photo){
+    FileSaver.saveAs(photo, `download-${_id}.jpg`);
 }
