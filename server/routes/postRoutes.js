430077c4324096e4f5bcb2529c5109737a424301
lomowang <frees1102@gmail.com>
2023-05-05 00:47:58 +0800

    20230505

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -18,11 +18,11 @@ router.route('/').get(async (req, res) => {
   try {
     const posts = await Post.find({});
     res.status(200).json({ success: true, data: posts });
-  } catch (err) {
-    res.status(500).json({ success: false, message: 'Fetching posts failed, please try again' });
+  } catch (error) {
+    res.status(500).json({ success: false, message:error });
   }
 });
-
+    
 router.route('/').post(async (req, res) => {
   try {
     const { name, prompt, photo } = req.body;
@@ -34,9 +34,9 @@ router.route('/').post(async (req, res) => {
       photo: photoUrl.url,
     });
 
-    res.status(200).json({ success: true, data: newPost });
-  } catch (err) {
-    res.status(500).json({ success: false, message: 'Unable to create a post, please try again' });
+    res.status(201).json({ success: true, data: newPost });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error });
   }
 });
 
